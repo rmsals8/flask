898cfa278200f874c374980d2893e5349c7bdf1b
Genu Min Na <96032260+rmsals8@users.noreply.github.com>
2024-10-16 22:57:25 +0900

    Update kakao_utils.py

--- app/kakao_utils.py
+++ app/kakao_utils.py
@@ -3,11 +3,19 @@ import hashlib
 import hmac
 import base64
 
+
 def verify_kakao_signature(request):
     try:
-        timestamp = request.headers['X-Kakao-Signature']
-        signature = base64.b64encode(hmac.new(os.environ['KAKAO_API_KEY'].encode('utf-8'), timestamp.encode('utf-8'), hashlib.sha256).digest()).decode('utf-8')
-        return signature == request.headers.get('X-Kakao-Signature')
+        timestamp = request.headers['X-Kakao-Timestamp']
+        signature = request.headers['X-Kakao-Signature']
+        computed_signature = base64.b64encode(
+            hmac.new(
+                os.environ['KAKAO_API_KEY'].encode('utf-8'),
+                timestamp.encode('utf-8'),
+                hashlib.sha256
+            ).digest()
+        ).decode('utf-8')
+        return signature == computed_signature
     except Exception as e:
-        print(f"카카오 서명 검증 중 오류 발생: {str(e)}")
+        logger.error(f"카카오 서명 검증 중 오류 발생: {str(e)}")
         return False
