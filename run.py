ab6d9dd02df623d4e49ba9141fd669ff2caa10cd
Genu Min Na <96032260+rmsals8@users.noreply.github.com>
2024-10-16 21:45:26 +0900

    Update run.py

--- run.py
+++ run.py
@@ -21,4 +21,4 @@ def create_admin():
         print(f"Admin user {username} created successfully.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
