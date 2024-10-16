5f06a7cc877a79976f2ed609049a79dd504c162d
Genu Min Na <96032260+rmsals8@users.noreply.github.com>
2024-10-16 22:07:29 +0900

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///chatbot.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    KAKAO_API_KEY = os.environ.get('KAKAO_API_KEY')
